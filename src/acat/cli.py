b9bde1ffd40770e7e91b91f8a36ba69a0288fa3f
Adrian Carreno <AdrianCarreno@users.noreply.github.com>
2025-03-22 22:26:41 -0300

    docs(cli): add tab autocompletion instructions to CLI documentation

--- src/acat/cli.py
+++ src/acat/cli.py
@@ -9,6 +9,10 @@ def cli():
     """Adrian Carreno's AWS Toolkit.
 
     This is a collection of tools that I use to manage my AWS resources.
+
+    To enable tab autocompletion, add the following line to your .bashrc or .zshrc:
+
+        eval "$(_ACAT_COMPLETE=source acat)"
     """
     pass
 
